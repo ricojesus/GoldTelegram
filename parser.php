73af03920ad0906cd70911cb5ef4353c4b487afc
RicardoJesus <ricojesus@hotmail.com>
2016-07-29 21:34:58 -0300

    Versão 1 do Bot da Gold

---
+++ parser.php
@@ -0,0 +1,178 @@
+<?php
+/**
+*
+* @author     Ricardo Jesus
+* @version    1.0  
+*  
+* Programa responsável por transformar a requisição num resultado em padrão texto.
+*
+* Versão Data       Autor            Descricao
+* ------ ---------- ---------------- --------------------------------------
+* 1.0    23/07/2016 Ricardo Jesus	 Versão inicial
+* 1.1	 29/07/2016	Tiago 			 Melhorias gerais e inclusao do Vatsim
+* ------ ---------- ---------------- --------------------------------------
+* 
+*/
+
+define('DEFAULT_FOOTER', "\n Sempre que precisar de auxilio me chame digitando /ajuda \n by @GodVirtualBot - goldvirtual.com.br");
+
+//Metodo principal responsavel por direcionar a requisicao para o metodo correspondente
+function getResult($mensagem, $text){
+	$out = '';
+
+	if($mensagem=="start"){
+		$out = getStart($text);
+	} elseif($mensagem=="metar"){
+		$out = getMetar(substr($text, 7));
+	} elseif($mensagem=="regras"){
+		$out = getRegras();
+	} elseif($mensagem=="ajuda"){
+		$out = getAjuda();
+	} elseif($mensagem=="vatsim"){
+		$out = getVatsim();
+	}
+	return $out;
+}
+
+function getStart($userName){
+	$resultado = "";
+
+	$resultado = "Olá <b>" . $userName . "</b>, cordial boa!\n";
+	$resultado .= "Bem vindo ao Grupo da Gold Virtual Airlines no Telegram. \n";
+	$resultado .= "Aqui você poderá conversar, se informar e se divertir com assuntos sobre aviação real e virtual. \n\nPara se informar das regras e das facilidades automatizadas que estão disponíveis siga 2 passos abaixo: \n";
+	$resultado .= "1)Clique aqui @GodVirtualBot \n";
+	$resultado .= "2)Digite /regras \n";
+	
+	return $resultado .= DEFAULT_FOOTER;
+}
+
+//Metodo para captura de Metar e TAF
+function getMetar($icao){
+	try{
+		$resultado = '';
+		
+		if($icao === false)
+		{
+			$resultado = "Olá, informe um ICAO válido para que eu consiga te mostrar o METAR e o TAF \n (Ex.: /metar SBRF) \n";
+			$resultado .= DEFAULT_FOOTER;
+			return $resultado;
+		}
+		else{		
+			$metar = file_get_contents('http://www.redemet.aer.mil.br/api/consulta_automatica/index.php?local='. $icao .'&msg=metar');
+
+			if (strpos($metar, 'localizada') == 0){
+				$resultado = 'Olá, veja como está o METAR de '. strtoupper($icao) . ' neste momento!';
+				$resultado .= $metar . " \n";
+				$resultado .= 'trouxe também o TAF com as previsões das próximas horas!';
+
+				$taf = file_get_contents('http://www.redemet.aer.mil.br/api/consulta_automatica/index.php?local='. $icao .'&msg=taf');
+
+				$resultado .=  $taf;
+				$resultado .= " \n" .DEFAULT_FOOTER;
+			}else{
+				$resultado = "Olá, eu sou o BOT da Gold Virtual Airlines! informe um ICAO válido para que eu consiga te informar o METAR \n Ex.: /metar SBRF \n \n";
+				$resultado .= DEFAULT_FOOTER;
+			}
+		}
+		return $resultado;
+
+	}catch (Exception $e){
+		return 'Erro consultando Metar, favor consultar a staff de TI da Gold' . $e->getMessage();
+	}
+}
+
+function getRegras(){
+	$resultado = "";
+
+	$resultado = "<b>*** Regras do Grupo ***</b>\n\n";
+	$resultado .= "* A informação, instrução, respeito, aviação e diversão, deverão ser os principios de qualquer interação no grupo. \n";
+	$resultado .= "* Proibido qualquer ato descriminatório como religião, cor, raça, etnia, politica e opção sexual. \n";
+	$resultado .= "* Proibido o compartilhamento de conteúdos piratas. \n";
+	$resultado .= "* Este canal é regido pelo Código de Ética e Conduta da Gold Virtual Airlines publicado no site da VA. \n";
+	
+	return $resultado .= DEFAULT_FOOTER;
+}
+
+function getAjuda(){
+	$resultado = "";
+
+	$resultado = "Olá, eu sou o BOT da Gold Virtual Airlines! \n\n";
+	$resultado .= "Veja abaixo todos os comandos e funcionalidades que estão disponiveis: \n";
+	$resultado .= "/ajuda - Comando para ver as funcionalidades do BOT \n";
+	$resultado .= "/regras - Comando para ver as Regras do Grupo \n";
+	$resultado .= "/metar - Comando para visualizar o metar e TAF \n";
+	$resultado .= "/vatsim - Comando para visualizar os Controladores Online na VATBRZ \n";
+	$resultado .= "\nSiga-nos nas Redes Sociais: \n";
+	$resultado .= "Facebook: www.facebook.com/GOLDVIRTUAL \n";
+	$resultado .= "Youtube: www.youtube.com/user/GoldVirtualAirlines \n";
+	$resultado .= "Telegram: www.telegram.me/goldvirtual \n";
+	$resultado .= "Twitter: www.twitter.com/GoldVirtualAirlines \n";
+	
+	return $resultado .= DEFAULT_FOOTER;
+}
+
+function getVatsim(){
+	//Busca Tabela
+	$url = file_get_contents('https://stats.vatsim.net/who.html');
+	$pos = strpos($url,'<h2>Controllers</h2>');
+	$tabela_ini = substr($url, $pos, strlen($url));
+	$pos_fim = strpos($tabela_ini,'</table>');
+	$tabela_fim = substr($tabela_ini, 1, $pos_fim);
+	//---
+	 
+	 //Busca Linha
+	$pos = strpos($tabela_fim, '<tr class="odd">');
+	$tabela = substr($tabela_fim, $pos);
+	$tabela = str_replace('<tr class="odd">','<tr>',$tabela);
+	//--
+	$retorno = '';
+	 
+	$processa = true;
+	while ($processa){
+		$pos_ini = strpos($tabela,'<tr>');
+		$pos_fim = strpos($tabela,'</tr>');
+		$linha = substr($tabela, $pos_ini,($pos_fim+5));
+		$tabela = str_replace($linha,'',$tabela);
+		$linha = str_replace('<tr>','',$linha);
+		$linha = str_replace('</tr>','',$linha);
+		$registro = '';
+	
+		for ($i = 1; $i <= 7; $i++) {
+			$pos_linha_ini = strpos($linha,'<td');
+			$pos_linha_fim = strpos($linha,'</td');
+			$campo=  substr($linha, $pos_linha_ini,$pos_linha_fim); 
+			$linha = str_replace($campo,'',$linha);
+			$campo = str_replace('<td>', '', $campo);
+			$campo = str_replace('</td>', '', $campo);
+			$campo = str_replace('</a>', '', $campo);
+			$pos_a = strpos($campo, '>');
+		
+			if (!($pos_a === false)) {
+				$campo = substr($campo, $pos_a+1, strlen($campo));
+			}
+		
+			if($i == 1 || $i==2 ){
+				$registro .= clean($campo) . ' - ';   
+			}elseif($i==6){
+			   $registro .= clean($campo);
+			}
+		}
+		if (substr($registro,0,1) == 'S'){
+			$retorno .= $registro . "\n\n";
+		}
+		
+		if ($pos_ini === false) {
+			$processa = false;
+		} 
+	}
+	if ($retorno == ''){
+		$retorno = 'Não temos controladores onlines no momento';
+	}
+	return $resultado = "A Gold Virtual Airlines informa os controladores Online na rede VATSIM: \n". $retorno. "\n Sempre que precisar de auxilio me chame digitando /ajuda \n by @GodVirtualBot - www.goldvirtual.com.br";
+}
+
+function clean($text){
+	$text = trim( preg_replace( '/\s+/', ' ', $text ) );  
+	$text = preg_replace("/(\r\n|\n|\r|\t)/i", '', $text);
+	return $text;
+}
---
+++ virtualbot.php
@@ -0,0 +1,80 @@
+<?php
+/**
+*
+* @author     Ricardo Jesus
+* @version    1.0  
+*  
+* Programa responsável por receber a requisição a partir do Telegram client indentificar o comando
+*   e comunicar com a api do Telegram e retornar o resultado para o Telegram client
+*
+* Versão Data       Autor            Descricao
+* ------ ---------- ---------------- --------------------------------------
+* 1.0    23/07/2016 Ricardo Jesus	 Versão inicial
+* ------ ---------- ---------------- --------------------------------------
+* 
+*/
+require('parser.php');
+
+// Token é o codigo unico cedido pelo Telegram, somente alterar caso seja requisitado um novo Token ao Telegram 
+// ** Token teste Ticardo ** //
+//define('BOT_TOKEN', '269387957:AAH5K_-dRKlw0pQb07Ontu8NxBG83pr2xhU');
+// ** Token teste Tiago ** //
+define('BOT_TOKEN', '181646159:AAEagAKnu-cpgHwBBbyegPBAAT7NO63_dfc');
+
+
+// ** Token da GOLD para implementar no ambiente de produção essa linha deve ser descomentada e a linha superior comentada. ** //
+
+//define('BOT_TOKEN', '269999230:AAFcwOL7UCs0BsYHqoXVhy9V5KEsinTlNaw');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+
+//Metodo de entrada das requisições, interpreta os comandos e direciona para o Parse
+function processMessage($message) {
+	// processa a mensagem recebida
+	$message_id = $message['message_id'];
+	$chat_id = $message['chat']['id'];
+
+	if (isset($message['text'])) {
+		$text = $message['text'];//texto recebido na mensagem
+
+		if (strtolower(substr($text, 0, 6)) == "/start") {
+			$text = $message['from']['first_name'];
+			sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('start', $message['from']['first_name']),'parse_mode'=>'HTML'));
+		} elseif (strtolower(substr($text, 0, 6)) == "/metar") {
+			sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('metar', $text),'disable_web_page_preview'=>true));
+		} elseif (strtolower(substr($text, 0, 7)) == "/regras") {
+			sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('regras', $text),'disable_web_page_preview'=>true,'parse_mode'=>'HTML'));
+		} elseif (strtolower(substr($text, 0, 6)) == "/ajuda") {
+			sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('ajuda', $text),'disable_web_page_preview'=>true,'parse_mode'=>'HTML'));
+		} elseif (strtolower(substr($text, 0, 10)) == "/vatsim") {
+			sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => getResult('vatsim', $text),'disable_web_page_preview'=>true,'parse_mode'=>'HTML'));
+		} else {
+			sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, '. $message['from']['first_name']. ' não consegui compreender sua mensagem!'));
+		}
+	} else {
+		sendMessage("sendMessage", array('chat_id' => $chat_id, "text" => 'Desculpe, mas só compreendo mensagens em texto'));
+	}
+}
+
+// Metodo para envio do retorno da requisição, não modificar
+function sendMessage($method, $parameters) {
+	$options = array('http' => array(
+					 'method'  => 'POST',
+					 'content' => json_encode($parameters),
+					 'header'=>  "Content-Type: application/json\r\n" .
+					 "Accept: application/json\r\n"));
+
+	$context  = stream_context_create( $options );
+	file_get_contents(API_URL.$method, false, $context );
+}
+
+//obtém as atualizações do bot
+$update_response = file_get_contents(API_URL."getUpdates");
+$response = json_decode($update_response, true);
+$length = count($response["result"]);
+//obtém a última atualização recebida pelo bot
+$update = $response["result"][$length-1];
+
+if (isset($update["message"])) {
+ 	processMessage($update["message"]);
+}
+?>
